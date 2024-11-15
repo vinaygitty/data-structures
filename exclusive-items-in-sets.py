2a80f023ba267f92cf063c419c90b273c9e18f47
Vinay Babu Yella <60830655+vinaygitty@users.noreply.github.com>
2024-11-15 13:36:32 -0800

    Update exclusive-items-in-sets.py

--- exclusive-items-in-sets.py
+++ exclusive-items-in-sets.py
@@ -1,5 +1,9 @@
 """
-You're a data analyst for a universal retail conglomerate managing two clothing stores, let's call them Space Threads and Galaxy Garments. You have two lists named inventory1 and inventory2 representing the clothing items from these stores. Now, your prime mission is to figure out the clothes that are exclusive to each store. Note that the letter casing is not important, so items like "t-shirt" and "T-Shirt" are considered the same. Return all exclusive clothes sorted in alphabetical order.
+You're a data analyst for a universal retail conglomerate managing two clothing stores, let's call them Space Threads and Galaxy Garments.
+You have two lists named inventory1 and inventory2 representing the clothing items from these stores.
+Now, your prime mission is to figure out the clothes that are exclusive to each store.
+Note that the letter casing is not important, so items like "t-shirt" and "T-Shirt" are considered the same.
+Return all exclusive clothes sorted in alphabetical order.
 Keep in mind that we're dealing with text data here. The length of these lists can range from as few as no items to as many as a star cluster.
 """
 def exclusive_products(inventory1, inventory2):
